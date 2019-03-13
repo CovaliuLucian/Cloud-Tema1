474ca5ae5858560f05111af7467e6ec0284d2096
Covaliu Lucian <luciancovaliu41@gmail.com>
2019-03-13 20:22:37 +0200

    Exception handling 101

--- Tema 2/HttpHandler.py
+++ Tema 2/HttpHandler.py
@@ -23,45 +23,52 @@ class HttpHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        rpath, args = Utilities.get_args(self.path)
-        resp = Response(False, "Not found", 404)
-        components = rpath.split("/")
-        components = list(filter(lambda a: a != "", components))
-        if components[0] == "users":
-            if len(components) == 1:
-                resp = self.user_controller.get()
-            if len(components) == 2:
-                resp = self.user_controller.get(components[1])
-            if len(components) > 2 and components[2] == "orders":
-                user_id = components[1]
-                if len(components) == 3:
-                    resp = self.user_controller.get_order(user_id)
-                if len(components) == 4:
-                    resp = self.user_controller.get_order(user_id, components[3])
-                if len(components) > 4 and components[4] == "products":
-                    order_id = components[3]
-                    if len(components) == 5:
-                        resp = self.user_controller.get_products(user_id, order_id)
-                    if len(components) == 6:
-                        resp = self.user_controller.get_products(user_id, order_id, components[5])
+        try:
+            rpath, args = Utilities.get_args(self.path)
+            resp = Response(False, "Not found", 404)
+            components = rpath.split("/")
+            components = list(filter(lambda a: a != "", components))
+            if components[0] == "users":
+                if len(components) == 1:
+                    resp = self.user_controller.get()
+                if len(components) == 2:
+                    resp = self.user_controller.get(components[1])
+                if len(components) > 2 and components[2] == "orders":
+                    user_id = components[1]
+                    if len(components) == 3:
+                        resp = self.user_controller.get_order(user_id)
+                    if len(components) == 4:
+                        resp = self.user_controller.get_order(user_id, components[3])
+                    if len(components) > 4 and components[4] == "products":
+                        order_id = components[3]
+                        if len(components) == 5:
+                            resp = self.user_controller.get_products(user_id, order_id)
+                        if len(components) == 6:
+                            resp = self.user_controller.get_products(user_id, order_id, components[5])
 
-        if components[0] == "products":
-            if len(components) == 1:
-                resp = self.product_controller.get()
-            if len(components) == 2:
-                resp = self.product_controller.get(components[1])
+            if components[0] == "products":
+                if len(components) == 1:
+                    resp = self.product_controller.get()
+                if len(components) == 2:
+                    resp = self.product_controller.get(components[1])
 
-        print(resp.to_json())
+            print(resp.to_json())
 
-        if not resp.success:
-            self.send_response(resp.error)
+            if not resp.success:
+                self.send_response(resp.error)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(resp.data.encode())
+                return
+
+            self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(resp.data.encode())
+            self.wfile.write(resp.to_json())
             return
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(resp.to_json())
-        return
+        except Exception as e:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(str(e))
+            return
